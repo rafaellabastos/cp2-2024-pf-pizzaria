cd2100da1a639a9b25611e9d1a939680277b4fc6
LuizFFarias <rm99519@fiap.com.br>
2024-04-29 10:37:31 -0300

    Iniciando branch rm99519

--- src/main/java/br/com/fiap/pizzaria/domain/entity/Sabor.java
+++ src/main/java/br/com/fiap/pizzaria/domain/entity/Sabor.java
@@ -1,5 +1,8 @@
 package br.com.fiap.pizzaria.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +13,10 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "TB_SABOR")
 public class Sabor {
+
     private Long id;
     private String nome;
     private String descricao;
