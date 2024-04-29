1b6175c10db190144ef741dc3f1cd87b82824343
LuizFFarias <rm99519@fiap.com.br>
2024-04-29 17:47:32 -0300

    Arrumando classes request

--- src/main/java/br/com/fiap/pizzaria/domain/dto/request/PizzariaRequest.java
+++ src/main/java/br/com/fiap/pizzaria/domain/dto/request/PizzariaRequest.java
@@ -1,12 +1,17 @@
 package br.com.fiap.pizzaria.domain.dto.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record PizzariaRequest(
 
+        @Size(min = 2, max = 255, message = "A quantidade de caracteres deve ser entre 2 e 255")
         @NotNull(message = "O nome é obrigatório")
         String nome,
 
+        @Valid
+        @Size(min = 2, max = 255, message = "A quantidade de caracteres deve ser entre 2 e 255")
         @NotNull(message = "O cardápio é obrigatório")
         ProdutoRequest cardapio
 ) {
--- src/main/java/br/com/fiap/pizzaria/domain/dto/request/ProdutoRequest.java
+++ src/main/java/br/com/fiap/pizzaria/domain/dto/request/ProdutoRequest.java
@@ -1,20 +1,25 @@
 package br.com.fiap.pizzaria.domain.dto.request;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
 
 public record ProdutoRequest(
 
+        @Size(min = 2, max = 255, message = "A quantidade de caracteres deve ser entre 2 e 255")
         @NotNull(message = "O nome do produto é obrigatório")
         String nome,
 
+        @Valid
         @NotNull(message = "O sabor é obrigatório")
         AbstractRequest sabor,
 
         @NotNull(message = "O preco é obrigatório")
         BigDecimal preco,
 
+        @Valid
         AbstractRequest opcional
 
 
